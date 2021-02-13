8eaa271859588595d45d3324f1705626016f1c9f
Leandro <leandroxb1@hotmail.com>
2021-02-13 14:51:20 -0300

    caule v1 - not working properly on a few mobile devices

--- handlers.py
+++ handlers.py
@@ -1,11 +1,11 @@
-from methods import choose_answer
+from methods import choose_answer, choose_caule
 
 
 def start(update, context):
     context.bot.send_message(chat_id = update.effective_chat.id, text = "Oi, eu sou o Wanderley! manda um /meajuda pra eu saber como te ajudar, meu nobre!")
 
 def meajuda(update, context):
-    context.bot.send_message(chat_id = update.effective_chat.id, text = "Fala meu consagrado! Por enquanto é isso que eu faço:\n\n/opiniao - Ta na dúvida se deve ou não seguir com a ideia errada? Eu te dou a resposta!\n/ideia - Lanço a braba")
+    context.bot.send_message(chat_id = update.effective_chat.id, text = "Fala meu consagrado! Por enquanto é isso que eu faço:\n\n/opiniao - Ta na dúvida se deve ou não seguir com a ideia errada? Eu te dou a resposta!\n/ideia - Lanço a braba.\n/caule - Por sua conta e risco.")
 
 def opiniao(update, context):
     context.bot.send_message(chat_id = update.effective_chat.id, text = choose_answer())
@@ -13,5 +13,8 @@ def opiniao(update, context):
 def ideia(update, context):
     context.bot.send_message(chat_id = update.effective_chat.id, text = "Tô poucas ideia hj parceiro, foi mal.")
 
+def caule(update, context):
+    context.bot.send_message(chat_id = update.effective_chat.id, text = choose_caule())
+
 def unknown(update, context):
-    context.bot.send_message(chat_id = update.effective_chat.id, text = "Num fode mano, não sou deus não. Lança o /meajuda e me usa direito.")
+    context.bot.send_message(chat_id = update.effective_chat.id, text = "Ta loco?. Lança o /meajuda e me usa direito.")
